75f4a0d2ca7ed105096c050df120bb21e23bb63e
haimantika mitra <32809211+Haimantika@users.noreply.github.com>
2020-09-07 18:52:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from autofeat import FeatureSelector, AutoFeatRegressor
+from sklearn.pipeline import make_pipeline
+import pickle
+def main():
+    # Get the dataset from the users GitHub repository
+    dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSITORY"] +"/master/dataset.csv"
+    df = pd.read_csv(dataset_path)
+    print()
+    print(df.describe())
+
+for steps in range(5):
+    np.random.seed(55)
+    print("### AutoFeat with %i feateng_steps" % steps)
+    afreg = AutoFeatRegressor(verbose=1, feateng_steps=steps)
+    df = afreg.fit_transform(df_org, target)
+    r2 = afreg.score(df_org, target)
+    print("## Final R^2: %.4f" % r2)
+    plt.figure()
+    plt.scatter(afreg.predict(df_org), target, s=2);
+    plt.title("%i FE steps (R^2: %.4f; %i new features)" % (steps, r2, len(afreg.new_feat_cols_)))
+afreg = AutoFeatRegressor(verbose=1, feateng_steps=3)
+# train on noisy data
+df = afreg.fit_transform(df_org, target_noisy)
+# test on real targets
+print("Final R^2: %.4f" % afreg.score(df, target))
+plt.figure()
+plt.scatter(afreg.predict(df), target, s=2);
+afreg = AutoFeatRegressor(verbose=1, feateng_steps=3)
+# train on noisy data
+df = afreg.fit_transform(df_org, target_very_noisy)
+# test on real targets
+print("Final R^2: %.4f" % afreg.score(df, target))
+plt.figure()
+plt.scatter(afreg.predict(df), target, s=2);
+
+if pipe:
+    pickle.dump(pipe,open('model.pkl','wb')) # store the artifact in docker container
+
+    if not os.environ["INPUT_MYINPUT"] == 'zeroinputs':
+        inputs = ast.literal_eval(os.environ["INPUT_MYINPUT"])
+        print("\nThe Predicted Ouput is :")
+        output = pipe.predict([inputs])
+        print(output)
+    else:
+        output = ["None"]
+        print("\nUser didn't provided inputs to predict")
+    
+    print("\n=======================Action Completed========================")
+    print(f"::set-output name=myOutput::{output[0]}")
+
+    
+
+
+if __name__ == "__main__":
+    main()
