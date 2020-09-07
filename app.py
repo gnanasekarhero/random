f712cc68a38af37ef10b7995f4e745c51b972954
Haimantika Mitra <haimantikamitra@gmail.com>
2020-09-07 20:50:00 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from autofeat import FeatureSelector, AutoFeatRegressor
@@ -7,35 +8,62 @@ import pickle
 def main():
     # Get the dataset from the users GitHub repository
     dataset_path = "https://raw.githubusercontent.com/" + os.environ["GITHUB_REPOSITORY"] +"/master/dataset.csv"
-    df = pd.read_csv(dataset_path)
+    data = pd.read_csv(dataset_path)
     print()
-    print(df.describe())
-
-for steps in range(5):
-    np.random.seed(55)
-    print("### AutoFeat with %i feateng_steps" % steps)
-    afreg = AutoFeatRegressor(verbose=1, feateng_steps=steps)
-    df = afreg.fit_transform(df_org, target)
-    r2 = afreg.score(df_org, target)
-    print("## Final R^2: %.4f" % r2)
-    plt.figure()
-    plt.scatter(afreg.predict(df_org), target, s=2);
-    plt.title("%i FE steps (R^2: %.4f; %i new features)" % (steps, r2, len(afreg.new_feat_cols_)))
-afreg = AutoFeatRegressor(verbose=1, feateng_steps=3)
-# train on noisy data
-df = afreg.fit_transform(df_org, target_noisy)
-# test on real targets
-print("Final R^2: %.4f" % afreg.score(df, target))
-plt.figure()
-plt.scatter(afreg.predict(df), target, s=2);
-afreg = AutoFeatRegressor(verbose=1, feateng_steps=3)
-# train on noisy data
-df = afreg.fit_transform(df_org, target_very_noisy)
-# test on real targets
-print("Final R^2: %.4f" % afreg.score(df, target))
-plt.figure()
-plt.scatter(afreg.predict(df), target, s=2);
-
+    print(data.describe())
+def test_model(dataset, model, param_grid):
+    # load data
+    X, y, _ = load_classification_dataset(dataset)
+    # split in training and test parts
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+    if model.__class__.__name__ == "SVC":
+        sscaler = StandardScaler()
+        X_train = sscaler.fit_transform(X_train)
+        X_test = sscaler.transform(X_test)
+    # train model on train split incl cross-validation for parameter selection
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        gsmodel = GridSearchCV(model, param_grid, cv=5)
+        gsmodel.fit(X_train, y_train)
+    print("best params:", gsmodel.best_params_)
+    print("best score:", gsmodel.best_score_)
+    print("Acc. on training data:", accuracy_score(y_train, gsmodel.predict(X_train)))
+    print("Acc. on test data:", accuracy_score(y_test, gsmodel.predict(X_test)))
+    return gsmodel.best_estimator_
+def test_autofeat(dataset, feateng_steps=2):
+    # load data
+    X, y, units = load_classification_dataset(dataset)
+    # split in training and test parts
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+    # run autofeat
+    afreg = AutoFeatClassifier(verbose=1, feateng_steps=feateng_steps, units=units)
+    # fit autofeat on less data, otherwise ridge reg model with xval will overfit on new features
+    X_train_tr = afreg.fit_transform(X_train, y_train)
+    X_test_tr = afreg.transform(X_test)
+    print("autofeat new features:", len(afreg.new_feat_cols_))
+    print("autofeat Acc. on training data:", accuracy_score(y_train, afreg.predict(X_train_tr)))
+    print("autofeat Acc. on test data:", accuracy_score(y_test, afreg.predict(X_test_tr)))
+    # train rreg on transformed train split incl cross-validation for parameter selection
+    print("# Logistic Regression")
+    rreg = LogisticRegression(class_weight="balanced")
+    param_grid = {"C": np.logspace(-4, 4, 10)}
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        gsmodel = GridSearchCV(rreg, param_grid, cv=5)
+        gsmodel.fit(X_train_tr, y_train)
+    print("best params:", gsmodel.best_params_)
+    print("best score:", gsmodel.best_score_)
+    print("Acc. on training data:", accuracy_score(y_train, gsmodel.predict(X_train_tr)))
+    print("Acc. on test data:", accuracy_score(y_test, gsmodel.predict(X_test_tr)))
+    print("# Random Forest")
+    rforest = RandomForestClassifier(n_estimators=100, random_state=13)
+    param_grid = {"min_samples_leaf": [0.0001, 0.001, 0.01, 0.05, 0.1, 0.2]}
+    gsmodel = GridSearchCV(rforest, param_grid, cv=5)
+    gsmodel.fit(X_train_tr, y_train)
+    print("best params:", gsmodel.best_params_)
+    print("best score:", gsmodel.best_score_)
+    print("Acc. on training data:", accuracy_score(y_train, gsmodel.predict(X_train_tr)))
+    print("Acc. on test data:", accuracy_score(y_test, gsmodel.predict(X_test_tr)))
 if pipe:
     pickle.dump(pipe,open('model.pkl','wb')) # store the artifact in docker container
 
