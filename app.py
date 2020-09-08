ca715a4e2936dbec2d60d0c004f2fb691ad3a70f
Haimantika Mitra <haimantikamitra@gmail.com>
2020-09-08 15:10:24 +0530

    update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def main():
     dataset = pd.read_csv(dataset_path)
     print()
     print(dataset.describe())
-    test_model(dataset)
+    test_model(dataset,model,param_grid)
     test_autofeat(dataset)
 
 def test_model(dataset, model, param_grid):
