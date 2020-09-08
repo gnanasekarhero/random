34fc361a772602c2b62fe462ccb8aa72c66ec4e0
Haimantika Mitra <haimantikamitra@gmail.com>
2020-09-08 19:13:47 +0530

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.datasets import dataset_path
+#from sklearn.datasets import dataset_path
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
