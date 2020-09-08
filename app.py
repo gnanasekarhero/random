f008dfbaf961a39d4dbce2ccb1672f57b2c68a55
Haimantika Mitra <haimantikamitra@gmail.com>
2020-09-08 15:42:16 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-from sklearn.datasets import dataset
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
